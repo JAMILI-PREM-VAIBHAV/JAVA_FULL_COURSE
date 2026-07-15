f535de75893693b601402ec49eb382518e8c9ca6
JAMILI-PREM-VAIBHAV <premvaibhav0802@gmail.com>
2026-07-15 23:04:54 +0530

    Added Java basic concepts

---
+++ DataType.java
@@ -0,0 +1,17 @@
+//Primitive DataTypes
+class DataType{
+    public static void main(String args[]){
+        //Integer
+        byte num=1;
+        short num1=10;
+        int num2=100;
+        long l=200l;
+        //Float
+        float f1=10.203f;
+        double f2=10.200000;
+        //char
+        char ch='P';
+        //boolean
+        boolean val=true;
+    }
+}
---
+++ Variable.java
@@ -0,0 +1,11 @@
+class Variable{
+    public static void main(String args[]){
+        //Syntax
+        // datatype variable_name=value;
+        int num=10;
+        System.out.println("int is type");
+        System.out.println("num is variable_name");
+        System.out.println("10 is value");
+        System.out.println(num);
+    }
+}
