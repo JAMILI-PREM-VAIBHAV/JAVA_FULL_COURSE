d7ff16eedb61a0d8ebe0728d83de38be39d95faf
JAMILI-PREM-VAIBHAV <premvaibhav0802@gmail.com>
2026-07-30 13:41:38 +0530

    Added Array Basic Problems

--- Arrays/SecondLargest.java
+++ Arrays/SecondLargest.java
@@ -6,25 +6,28 @@ class SecondLargest{
         System.out.println("Enter array size: ");
         int size=sc.nextInt();
         int arr[]=new int[size];
+        if(size<2){
+            System.out.println("No largest element");
+        }
         System.out.println("Enter array elements: ");
         for(int i=0;i<size;i++){
             arr[i]=sc.nextInt();
         }
-        System.out.println("Array elements are before sorting: ");
+        System.out.println("Array elements before sorting are: ");
         for(int i=0;i<size;i++){
             System.out.print(arr[i]+" ");
         }
-        System.out.println();
         Arrays.sort(arr);
+        System.out.println();
         System.out.println("Array elements after sorting: ");
         for(int i=0;i<size;i++){
             System.out.print(arr[i]+" ");
         }
         System.out.println();
-        System.out.println("Second largest element is: ");
-        for(int i=size-1;i>=size-2;i--){
-            if(i==size-2)
-            System.out.print(arr[i]+" ");
+        for(int i=size-1;i>=0;i--){
+            if(i==size-2){
+                System.out.print(arr[i]);
+            }
         }
     }
 }
